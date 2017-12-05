3315970601f2968efcd60f20a4a5ca42285225ab
ian <iansangines@gmail.com>
2017-12-05 18:00:56 +0100

    Start implementing home webpage

--- exploratory-TravelAgencyGoWebApplication/src/handlers/handlers.go
+++ exploratory-TravelAgencyGoWebApplication/src/handlers/handlers.go
@@ -2,9 +2,25 @@ package handlers
 
 import (
 	"net/http"
-	"fmt"
+	"html/template"
+
 )
 
-func HomeHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w,"Welcome to the Home! =)")
+type User struct {
+	Username string
 }
+
+func HomeHandler(w http.ResponseWriter, r *http.Request) {
+
+	t, _ := template.ParseFiles("templates/home.html")
+		u := User{Username : "whtever"}
+		//p := Page{Title: "Primera pagina", Body: "Haura sortit be?"}
+		t.Execute(w, u)
+
+
+	//fmt.Fprintln(w,"Welcome to the Home! =)")
+}
+
+
+
+
