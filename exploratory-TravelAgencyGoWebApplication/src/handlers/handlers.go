749182c1b67ed6c6c1435a4e2eef69fc52cbbebd
iansangines <iansangines@gmail.com>
2017-12-09 15:29:20 +0100

    Add login page and error control (404, 500)

--- exploratory-TravelAgencyGoWebApplication/src/handlers/handlers.go
+++ exploratory-TravelAgencyGoWebApplication/src/handlers/handlers.go
@@ -3,24 +3,36 @@ package handlers
 import (
 	"net/http"
 	"html/template"
-
+	"fmt"
 )
 
 type User struct {
 	Username string
 }
 
-func HomeHandler(w http.ResponseWriter, r *http.Request) {
 
-	t, _ := template.ParseFiles("templates/home.html")
-		u := User{Username : "whtever"}
-		//p := Page{Title: "Primera pagina", Body: "Haura sortit be?"}
-		t.Execute(w, u)
-
-
-	//fmt.Fprintln(w,"Welcome to the Home! =)")
+func HomeHandler(w http.ResponseWriter, r *http.Request) (int, error) {
+	if r.Method == "POST" {
+		//error?
+	}
+	t, _ := template.ParseGlob("templates/*")
+	u := User{Username : "whtever"}
+	return http.StatusOK, t.ExecuteTemplate(w, "hompe.html" ,u)
 }
 
+func LoginHandler(w http.ResponseWriter, r *http.Request) (int, error) {
+
+	t, _ := template.ParseGlob("templates/*")
+	logged := false
+	if logged {
+		http.Redirect(w, r, "/home", http.StatusSeeOther)
+	}
+	return http.StatusOK, t.ExecuteTemplate(w, "login.html", nil)
+}
+
+func NotFoundHandler(w http.ResponseWriter, r *http.Request){
+	fmt.Fprint(w, "error 404 :)")
+}
 
 
 
--- exploratory-TravelAgencyGoWebApplication/src/routes/router.go
+++ exploratory-TravelAgencyGoWebApplication/src/routes/router.go
@@ -3,12 +3,35 @@ package routes
 import (
 	"github.com/gorilla/mux"
 	"handlers"
+	"net/http"
 )
+
+type appHandler func(http.ResponseWriter, *http.Request) (int, error)
+
+func (fn appHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	if status, err := fn(w, r); err != nil {
+		switch status {
+		// We can have cases as granular as we like, if we wanted to
+		// return custom errors for specific status codes.
+		case http.StatusNotFound:
+			handlers.NotFoundHandler(w,r)
+		case http.StatusInternalServerError:
+			http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+		default:
+			// Catch any other errors we haven't explicitly handled
+			http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+		}
+	}
+}
 
 func GetRouter() *mux.Router {
 
+
+
 	router := mux.NewRouter()
-	router.HandleFunc("/home", handlers.HomeHandler)
+	router.Handle("/home", appHandler(handlers.HomeHandler)).Methods("GET")
+	router.NotFoundHandler = http.HandlerFunc(handlers.NotFoundHandler)
+	router.Handle("/login", appHandler(handlers.LoginHandler))
 
 	return router
 }
