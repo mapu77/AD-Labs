9989e7862cfbe18b3470a8826cc1c9d7fe6fb5cd
iansangines <iansangines@gmail.com>
2017-12-09 18:32:08 +0100

    Implement session and error in logout

--- exploratory-TravelAgencyGoWebApplication/src/handlers/handlers.go
+++ exploratory-TravelAgencyGoWebApplication/src/handlers/handlers.go
@@ -3,13 +3,17 @@ package handlers
 import (
 	"net/http"
 	"html/template"
-	"fmt"
+	"github.com/gorilla/sessions"
+	"log"
 )
 
 type User struct {
 	Username string
+	Password string
 }
 
+var cookie = sessions.NewCookieStore([]byte("travelAgencyCookie"))
+var users = [2]User{{Username: "Edu", Password: "1234"},{Username: "Ian", Password:"1234"}}
 
 func HomeHandler(w http.ResponseWriter, r *http.Request) (int, error) {
 	if r.Method == "POST" {
@@ -17,22 +21,75 @@ func HomeHandler(w http.ResponseWriter, r *http.Request) (int, error) {
 	}
 	t, _ := template.ParseGlob("templates/*")
 	u := User{Username : "whtever"}
-	return http.StatusOK, t.ExecuteTemplate(w, "hompe.html" ,u)
+	return http.StatusOK, t.ExecuteTemplate(w, "home.html" ,u)
 }
 
 func LoginHandler(w http.ResponseWriter, r *http.Request) (int, error) {
 
-	t, _ := template.ParseGlob("templates/*")
-	logged := false
-	if logged {
-		http.Redirect(w, r, "/home", http.StatusSeeOther)
+	if r.Method == "POST"{
+
+		err := r.ParseForm()
+		if err != nil {
+			log.Fatal(err)
+		}
+		username := r.PostFormValue("username")
+		password := r.PostFormValue("password")
+
+		exists := false
+		for _, user := range users{
+			if user.Username == username && user.Password == password{
+				exists = true
+				break
+			}
+		}
+
+		if exists {
+			session, err := cookie.Get(r, "travelAgencySession")
+			if err != nil {
+				http.Error(w, err.Error(), http.StatusInternalServerError)
+			}
+
+			session.Values["username"] = username
+			session.Save(r,w)
+			http.Redirect(w, r, "/home", http.StatusSeeOther)
+		} else {
+			http.Error(w, "bad username or password", http.StatusBadRequest)
+		}
 	}
-	return http.StatusOK, t.ExecuteTemplate(w, "login.html", nil)
+
+	if r.Method == "GET" {
+		session, err := cookie.Get(r, "travelAgencySession")
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+		}
+
+		logged := session.Values["username"] != nil
+		if logged {
+			http.Redirect(w, r, "/home", http.StatusSeeOther)
+		}
+
+		t, _ := template.ParseGlob("templates/*")
+		return http.StatusOK, t.ExecuteTemplate(w, "login.html", nil)
+	}
+	return http.StatusMethodNotAllowed, nil
 }
 
 func NotFoundHandler(w http.ResponseWriter, r *http.Request){
-	fmt.Fprint(w, "error 404 :)")
+	t, _ := template.ParseGlob("templates/*")
+	t.ExecuteTemplate(w, "404.html", nil)
 }
 
+func LogoutHandler(w http.ResponseWriter, r *http.Request) (int, error){
+	if r.Method == "GET" {
+		session, err := cookie.Get(r, "travelAgencySession")
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+		}
 
+		session.Values["username"] = nil
+
+		http.Redirect(w,r,"/login", http.StatusOK)
+	}
+	return http.StatusOK, nil
+}
 
--- exploratory-TravelAgencyGoWebApplication/src/routes/router.go
+++ exploratory-TravelAgencyGoWebApplication/src/routes/router.go
@@ -4,11 +4,28 @@ import (
 	"github.com/gorilla/mux"
 	"handlers"
 	"net/http"
+	"github.com/gorilla/sessions"
+
 )
 
 type appHandler func(http.ResponseWriter, *http.Request) (int, error)
 
 func (fn appHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+
+	cookie := sessions.NewCookieStore([]byte("travelAgencyCookie"))
+	session, err := cookie.Get(r, "travelAgencySession")
+	session.Options = &sessions.Options{
+		MaxAge:   3600,
+		HttpOnly: true,
+	}
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+	if session.Values["username"] == nil {
+		if r.URL.Path != "/login" {
+			http.Redirect(w, r, "/login", http.StatusSeeOther)
+		}
+	}
 	if status, err := fn(w, r); err != nil {
 		switch status {
 		// We can have cases as granular as we like, if we wanted to
@@ -32,6 +49,7 @@ func GetRouter() *mux.Router {
 	router.Handle("/home", appHandler(handlers.HomeHandler)).Methods("GET")
 	router.NotFoundHandler = http.HandlerFunc(handlers.NotFoundHandler)
 	router.Handle("/login", appHandler(handlers.LoginHandler))
+	router.Handle("/logout", appHandler(handlers.LogoutHandler))
 
 	return router
 }
