8fd194ff684bdc438f074a13a630641d18e1fa19
Eduard Maura <edu.maura.puig@gmail.com>
2017-12-12 17:22:29 +0100

    Add post and get /flights routes

--- exploratory-TravelAgencyGoAPI/src/core/Flight.go
+++ exploratory-TravelAgencyGoAPI/src/core/Flight.go
@@ -1,11 +1,13 @@
 package core
 
 import (
-	"fmt"
+	"gopkg.in/mgo.v2"
+	"log"
+	"gopkg.in/mgo.v2/bson"
 )
 
 type Flight struct {
-	Id            int
+	Id            bson.ObjectId `json:"id" bson:"_id,omitempty"`
 	Code          string
 	Company       string
 	DepartureTime string
@@ -14,13 +16,50 @@ type Flight struct {
 	ArrivalCity   string
 }
 
+// Returns the flight matching the parameters if specified, or the error if occurs.
+func ListBy(code string, company string, departureCity string, arrivalCity string) ([]Flight, error) {
+	session, err := mgo.Dial("localhost:27017")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer session.Close()
 
-//func newFlight(id int, code string, company string, departureTime string, departureCity string, arrivalTime string, arrivalCity string) *Flight {
-//	return &Flight{Id: id, Code: code, Company: company, DepartureTime: departureTime, DepartureCity: departureCity, ArrivalTime: arrivalTime, ArrivalCity: arrivalCity}
-//}
+	session.SetMode(mgo.Monotonic, true)
+
+	c := session.DB("ad-travel-agency").C("flights")
+	var args = bson.M{}
+	if code != "" {
+		args["code"] = code
+	}
+	if company != "" {
+		args["company"] = company
+	}
+	if company != "" {
+		args["departureCity"] = departureCity
+	}
+	if company != "" {
+		args["arrivalCity"] = arrivalCity
+	}
+	var flights []Flight
+	err = c.Find(args).Sort("_id").All(&flights)
+	return flights, err
+}
 
 // Make a flight persistent. Returns nil if ok, the produced error otherwise
-func (f *Flight) PersistFlight() error {
-	fmt.Print("Flight persisted")
-	return nil
+func (f Flight) PersistFlight() error {
+	session, err := mgo.Dial("localhost:27017")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer session.Close()
+
+	session.SetMode(mgo.Monotonic, true)
+
+	c := session.DB("ad-travel-agency").C("flights")
+	err = c.Insert(&f)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return err
 }
